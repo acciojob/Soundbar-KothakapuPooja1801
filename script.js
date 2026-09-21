27bf7e9b4759d0c4b15fd594962e49e8fde3f70d
Kothakapu Pooja <poojareddy5588@gmail.com>
2026-09-21 12:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let sounds = ['applause','boo','gasp','tada','victory','wrong','stop']
+let btns = document.getElementById('buttons')
+sounds.forEach((sound)=>{
+	let button = document.createElement("button")
+	button.className = "btn"
+	button.textContent = sound
+	button.addEventListener("click",()=>{
+		let audio = new Audio("./sounds/" + sound + ".mp3")
+		audio.play()
+	})
+	buttons.appendChild(button)
+})
+let stopBtn = document.createElement('button')
+stopBtn.className = "stop"
+stopBtn.textContent = "Stop"
+stopBtn.addEventListener("click",()=>{
+	location.reload()
+})
+buttons.appendChild(stopBtn)
